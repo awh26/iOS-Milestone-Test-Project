59ffbd8bc116cc0978daad0244b01088334a42fc
awh26 <alexander.hanna@greenwichschools.org>
2021-06-20 22:52:57 -0400

    chapter 1 complete

---
+++ hello world/hello world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello world
+//
+//  Created by Alex Hanna on 6/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hello world/hello world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello world
+//
+//  Created by Alex Hanna on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
