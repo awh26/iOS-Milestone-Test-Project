96fcaf6962c8062136185029dd39223fe81255d7
awh26 <alexander.hanna@greenwichschools.org>
2021-06-24 10:59:10 -0400

    lessons 2 and 3

---
+++ lesson02project/lesson02project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson02project
+//
+//  Created by Alex Hanna on 6/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
